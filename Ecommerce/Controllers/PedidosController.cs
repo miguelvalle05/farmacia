0b5adfcba75ffd1dc46f9d8242761c8e4614e872
Luis <black.hdr@hotmail.com>
2020-06-22 18:34:41 -0500

    Almacen Compras Recepcion

---
+++ Ecommerce/Controllers/PedidosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class PedidosController : Controller
+    {
+        // GET: Pedidos
+        public ActionResult Pedidos()
+        {
+            return View();
+        }
+
+        public ActionResult GenerarPedido()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ecommerce/Estilos/js/DataTable.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+    var table = $('#example').DataTable({
+        fixedHeader: true
+    });
+});
